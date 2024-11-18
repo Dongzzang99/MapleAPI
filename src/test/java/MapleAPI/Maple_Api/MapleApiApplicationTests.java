9b5322992668fd60eabc7e5ff58ab11cfa6961d0
dongzzang <kaka2172@naver.com>
2024-11-18 23:46:21 +0900

    first commit

---
+++ src/test/java/MapleAPI/Maple_Api/MapleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package MapleAPI.Maple_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
